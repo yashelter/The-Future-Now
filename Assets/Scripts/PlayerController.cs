10361129e99c9465396662029cb051b57aee7464
yashelter <yashelter@yandex.ru>
2020-11-29 15:56:48 +0300

    First Save

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public controlTypes controlType;
+    public float moveSpeed;
+    public float jumpForce;
+    [HideInInspector]
+    public enum controlTypes{
+        PC, Phone
+    }
+
+    private Animator playerAnimator;
+    private Transform PlayerTransform;
+    private Rigidbody2D playerRB;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if(controlType == controlTypes.PC)
+        {
+            PCMove();
+        }else if(controlType == controlTypes.Phone)
+        {
+
+        }
+    }
+    private void PCMove()
+    {
+        playerRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y, 0);
+    }
+}
