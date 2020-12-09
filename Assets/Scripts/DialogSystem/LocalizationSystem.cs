41f35d941f6496b312591a7b6e88eef20298ed75
yashelter <yashelter@yandex.ru>
2020-12-09 22:31:20 +0300

    added localization system

---
+++ Assets/Scripts/DialogSystem/LocalizationSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationSystem : MonoBehaviour
+{
+    public Dictionary<string, string> cSVtable;
+    private void Start()
+    {
+        UpdateDict();
+    }
+    public void UpdateDict()
+    {
+        int rowN = 0;
+        TextAsset table = Resources.Load<TextAsset>("fre");
+        string[] lines = table.text.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        if (!PlayerPrefs.HasKey("language"))
+        {
+            PlayerPrefs.SetString("language", "eng");
+        }
+        string[] line0 = lines[0].Split(new char[] { ';' }, System.StringSplitOptions.RemoveEmptyEntries);
+        for(int i = 0; i < line0.Length; i++)
+        {
+            if(line0[i] == PlayerPrefs.GetString("language"))
+            {
+                rowN = i;
+                break;
+            }
+        }
+        cSVtable = new Dictionary<string, string>();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] line = lines[i].Split(new char[] { ';' }, System.StringSplitOptions.RemoveEmptyEntries);
+            cSVtable.Add(line[0], line[rowN]);
+        }
+    }
+    public string GetKey(string key)
+    {
+        return cSVtable[key];
+    }
+
+}
