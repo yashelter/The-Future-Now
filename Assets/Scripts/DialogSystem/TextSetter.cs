d79856ced89755bda178bd9801e25ab4b2189208
yashelter <yashelter@yandex.ru>
2020-12-13 22:23:59 +0300

    Revert "added localization system"
    This reverts commit 41f35d941f6496b312591a7b6e88eef20298ed75.
    fixed localization system

---
+++ Assets/Scripts/DialogSystem/TextSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextSetter : MonoBehaviour
+{
+    public string key;
+    private TextMeshProUGUI text;
+    private LocalizationSystem lang;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        lang = FindObjectOfType<LocalizationSystem>();
+        lang.UpdateDict();
+        text.text = lang.GetKey(key);
+    }
+    public void UpdateText()
+    {
+        lang.UpdateDict();
+        text.text = lang.GetKey(key);
+    }
+
+}
